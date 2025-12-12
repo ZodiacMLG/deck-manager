f315854b71f566676a37f2fe1d3fb774a5bd90c4
ZodiacMLG <zodiacmlg@bk.ru>
2025-12-12 13:25:00 +0500

    Files with source code

---
+++ Decks.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckProject
+{
+    internal class Decks
+    {
+        struct Deck
+        {
+            public string Name_card;
+            public string Description;
+        }
+
+        Deck[] decks;
+        int sizeDeck;
+        private Random random = new Random();
+
+        enum Suits { Spades, Hearts, Diamonds, Clubs}
+        public Decks() { 
+            decks = new Deck[0];
+            sizeDeck = 0;
+        }
+        
+        public Decks (int n)
+        {
+            sizeDeck = n;
+            decks = new Deck[n];
+            int startDeck = 0;
+
+            if (n == 54 || n == 52)
+            {
+                startDeck = 2;
+            } else if (n == 36)
+            {
+                startDeck = 6;
+            }
+
+            int suits = n / 4;
+            var countSuits = Suits.Spades;
+            int count_cards = 0;
+            int temp = startDeck;
+
+            for (int i = 0; i <= (int)Suits.Clubs; i++) 
+            {
+                for (int j = 0; j < n / 4; j++)
+                {
+                    decks[count_cards].Name_card = startDeck.ToString();
+                    decks[count_cards].Description = countSuits.ToString();
+                    
+                    switch (startDeck)
+                    {
+                        case 11: decks[count_cards].Name_card = "Валет"; break;
+                        case 12: decks[count_cards].Name_card = "Дама"; break;
+                        case 13: decks[count_cards].Name_card = "Король"; break;
+                        case 14: decks[count_cards].Name_card = "Туз"; break;
+                    }
+
+                    count_cards++;
+                    startDeck++;
+                }
+                countSuits++;
+                startDeck = temp;
+            }
+
+            if (n == 54)
+            {
+                decks[52].Name_card = "Джокер";
+                decks[52].Description = "Joker";
+                decks[53].Name_card = "Джокер";
+                decks[53].Description = "Joker";
+            }
+
+        }
+
+        public void ShowCards()
+        {
+            foreach (var deck in decks)
+            {
+                Console.WriteLine($"{deck.Name_card} - {deck.Description}");
+            }
+        }
+
+        public void ShowCard(string name, string description)
+        {
+            bool find = false;
+            foreach (var deck in decks)
+            {
+                if (deck.Name_card == name && deck.Description == description)
+                {
+                    Console.WriteLine($"{deck.Name_card} - {deck.Description}");
+                    find = true;
+                }
+            }
+            if (!find)
+            {
+                Console.WriteLine($"Card - {name}:{description} - dont find");
+            }
+        }
+
+        public void Sort()
+        {
+            bool sorted = true;
+            Deck tempDeck;
+            do
+            {
+                sorted = true;
+                for (int j = 0; j < sizeDeck - 1; j++)
+                {
+                    if (GetCardWeight(decks[j].Name_card) < GetCardWeight(decks[j + 1].Name_card))
+                    {
+                        tempDeck = decks[j];
+                        decks[j] = decks[j + 1];
+                        decks[j + 1] = tempDeck;
+                        sorted = false;
+                    }
+                }
+            } while (!sorted);
+        }
+
+        public void Shuffle()
+        {
+            
+            Deck temp;
+            for (int i = decks.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                temp = decks[i];
+                decks[i] = decks[j];
+                decks[j] = temp;
+            }
+        }
+
+        public int CountCards() { return decks.Length; }
+
+        private int GetCardWeight(string name)
+        {
+
+            if (int.TryParse(name, out int number)){
+                return number;
+            }
+            else {
+                switch (name)
+                {
+                    case "Валет": return 11;
+                    case "Дама": return 12;
+                    case "Король": return 13;
+                    case "Туз": return 14;
+                    case "Джокер": return 15;
+                }
+            }
+            return 0;
+        }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace DeckProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Decks Deck;
+            Console.WriteLine("Enter decks: \n1 - 36\n2 - 52\n3 - 54");
+
+            string ?choice = Console.ReadLine();
+
+            for (; ; )
+            {
+                if (choice == "1")
+                {
+                    Deck = new Decks(36);
+                    break;
+                }
+                else if (choice == "2")
+                {
+                    Deck = new Decks(52);
+                    break;
+                }
+                else if (choice == "3")
+                {
+                    Deck = new Decks(54);
+                    break;
+                    
+                }
+                Console.Write("Enter correct value: ");
+                choice = Console.ReadLine();
+            }
+            Console.Clear();
+            
+            for(; ; )
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine($"Deck ({Deck.CountCards()})");
+                Console.WriteLine("1 - Show deck\n2 - Sort deck\n3 - Find card in deck\n4 - Shuffle\n5 - Exit");
+                Console.WriteLine("-------------------------------");
+                choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    Console.Clear();
+                    Deck.ShowCards();
+                }
+                else if (choice == "2")
+                {
+                    Console.Clear();
+                    Deck.Sort();
+                    Console.WriteLine("Sort complete.");
+                }
+                else if (choice == "3")
+                {
+                    Console.Clear();
+                    Console.WriteLine("What card do u want to find?");
+                    string? card = Console.ReadLine();
+                    string? cardDesc = Console.ReadLine();
+                    Deck.ShowCard(card, cardDesc);
+                } 
+                else if (choice == "4")
+                {
+                    Console.Clear();
+                    Deck.Shuffle();
+                    Console.WriteLine("Shuffle complete");
+                }
+                else if (choice == "5")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }
+        }
+    }
+}
